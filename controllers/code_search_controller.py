edccdb05009813d2a063e49161494a3d2c442a61
Laurin Stricker <laurin.stricker@compunet-it.com>
2022-03-11 17:11:12 +0100

    Continued with code-search NL-PL.

--- controllers/code_search_controller.py
+++ controllers/code_search_controller.py
@@ -1,7 +1,10 @@
 from flask import json
 from flask.wrappers import Request
 import torch
-from transformers import AutoModel, AutoTokenizer
+from torch.utils.data import DataLoader, TensorDataset, RandomSampler, SequentialSampler
+from transformers import RobertaConfig, RobertaTokenizer, RobertaForSequenceClassification
+import numpy as np
+from tqdm import tqdm
 
 class CodeSearchController:
 
@@ -15,16 +18,111 @@ class CodeSearchController:
         if not content or not search_text:
             return None
         
+        search_interval = 10
+        max_seq_length = 200
+        sequence_a_segment_id = 0
+        sequence_b_segment_id = 1
+        cls_token_segment_id = 1
+        pad_token = 0
+        pad_token_segment_id = 0
+        eval_batch_size = 8
+
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-        tokenizer = AutoTokenizer.from_pretrained("microsoft/codebert-base")
-        model = AutoModel.from_pretrained("microsoft/codebert-base")
+        config = RobertaConfig.from_pretrained("microsoft/codebert-base", num_labels=2, finetuning_task="codesearch")
+        tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
+        model = RobertaForSequenceClassification.from_pretrained("microsoft/codebert-base", config=config)
         model.to(device)
-        
-        nl_tokens = tokenizer.tokenize(search_text)
-        code_tokens = tokenizer.tokenize(content)
-        tokens = [tokenizer.cls_token]+nl_tokens+[tokenizer.sep_token]+code_tokens+[tokenizer.sep_token]
-        tokens_ids = tokenizer.convert_tokens_to_ids(tokens)
-        context_embeddings = model(torch.tensor(tokens_ids)[None,:])[0]
 
-        return { "result": context_embeddings }
+        features = []
+        lines = content.splitlines()
+        i = 0
+        while i < len(lines):
+            search_text_tokens = tokenizer.tokenize(search_text)[:50]
+            code = lines[i : (i+search_interval)]
+            code_tokens = tokenizer.tokenize(code)
+            self.truncate_seq_pair(search_text_tokens, code_tokens, max_seq_length-3)
+
+            tokens = search_text_tokens + [tokenizer.sep_token]
+            segment_ids = [sequence_a_segment_id] * len(tokens)
+
+            tokens += code_tokens + [tokenizer.sep_token]
+            segment_ids += [sequence_b_segment_id] * (len(code_tokens) + 1)
+
+            tokens = [tokenizer.cls_token] + tokens
+            segment_ids = [cls_token_segment_id] + segment_ids
+        
+            input_ids = tokenizer.convert_tokens_to_ids(tokens)
+            input_mask = [1] * len(input_ids)
+
+            padding_length = max_seq_length - len(input_ids)
+            input_ids = input_ids + ([pad_token] * padding_length)
+            input_mask = input_mask + ([0] * padding_length)
+            segment_ids = segment_ids + ([pad_token_segment_id] * padding_length)
+
+            features.append(
+            InputFeatures(input_ids=input_ids,
+                          input_mask=input_mask,
+                          segment_ids=segment_ids,
+                          label_id=0))
+
+            i += (search_interval + 1)
+
+        all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
+        all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
+        all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
+        all_label_ids = torch.tensor([f.label_id for f in features], dtype=torch.long)
+        dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
+
+        eval_sampler = SequentialSampler(dataset)
+        eval_dataloader = DataLoader(dataset, sampler=eval_sampler, batch_size=eval_batch_size)
+
+        eval_loss = 0.0
+        nb_eval_steps = 0
+        preds = None
+        out_label_ids = None
+        for batch in tqdm(eval_dataloader, desc="Evaluating"):
+            model.eval()
+            batch = tuple(t.to(device) for t in batch)
+
+            with torch.no_grad():
+                inputs = {'input_ids': batch[0],
+                          'attention_mask': batch[1],
+                          'token_type_ids': None,
+                          # XLM don't use segment_ids
+                          'labels': batch[3]}
+
+                outputs = model(**inputs)
+                tmp_eval_loss, logits = outputs[:2]
+
+                eval_loss += tmp_eval_loss.mean().item()
+            nb_eval_steps += 1
+            if preds is None:
+                preds = logits.detach().cpu().numpy()
+                out_label_ids = inputs['labels'].detach().cpu().numpy()
+            else:
+                preds = np.append(preds, logits.detach().cpu().numpy(), axis=0)
+                out_label_ids = np.append(out_label_ids, inputs['labels'].detach().cpu().numpy(), axis=0)
+
+        eval_loss = eval_loss / nb_eval_steps
+        preds_label = np.argmax(preds, axis=1)
+        all_logits = preds.tolist()
+
+        return { "result": all_logits }
+
+
+    def truncate_seq_pair(self, tokens_a, tokens_b, max_length):
+        while True:
+            total_length = len(tokens_a) + len(tokens_b)
+            if total_length <= max_length:
+                break
+            if len(tokens_a) > len(tokens_b):
+                tokens_a.pop()
+            else:
+                tokens_b.pop()
+
+class InputFeatures(object):
+    def __init__(self, input_ids, input_mask, segment_ids, label_id):
+        self.input_ids = input_ids
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+        self.label_id = label_id
