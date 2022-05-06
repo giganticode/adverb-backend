1d6bb248f87c0728fb0f56622c3f61f129782bdd
Laurin Stricker <laurin.stricker@gmail.com>
2022-05-06 12:17:04 +0200

    Changes for ironspeed.

--- webservice.py
+++ webservice.py
@@ -1,4 +1,3 @@
-import logging
 import os 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
@@ -13,10 +12,10 @@ from flask import json
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-from controllers.code_summary_controller import CodeSummaryController
-from controllers.code_symbol_controller import CodeSymbolController
-from controllers.code_search_codebert_controller import CodeSearchCodeBertController
-from controllers.code_search_colbert_controller import CodeSearchColBertController
+# from controllers.code_summary_controller import CodeSummaryController
+# from controllers.code_symbol_controller import CodeSymbolController
+# from controllers.code_search_codebert_controller import CodeSearchCodeBertController
+# from controllers.code_search_colbert_controller import CodeSearchColBertController
 
 DEBUG = True
 PORT = 8090
@@ -26,66 +25,66 @@ app = Flask(__name__, static_folder="")
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 # controllers
-code_summary = CodeSummaryController()
-code_symbol = CodeSymbolController()
-code_search_code_bert = CodeSearchCodeBertController()
-code_search_col_bert = CodeSearchColBertController()
+# code_summary = CodeSummaryController()
+# code_symbol = CodeSymbolController()
+# code_search_code_bert = CodeSearchCodeBertController()
+# code_search_col_bert = CodeSearchColBertController()
 
-# API routes
-@app.route("/api/summary", methods = ["POST"])
-def get_summary():
-    try:
-        summary = code_summary.get_summary(request)
-        if summary:
-            response = jsonify(summary)
-            response.headers.add("Access-Control-Allow-Origin", "*")
-            return response
-    except:
-        pass
-    return "Bad request", "400"
+# # API routes
+# @app.route("/api/summary", methods = ["POST"])
+# def get_summary():
+#     try:
+#         summary = code_summary.get_summary(request)
+#         if summary:
+#             response = jsonify(summary)
+#             response.headers.add("Access-Control-Allow-Origin", "*")
+#             return response
+#     except:
+#         pass
+#     return "Bad request", "400"
 
-@app.route("/api/name", methods = ["POST"])
-def get_symbol_name():
-    try:
-        name = code_symbol.get_symbol_name(request)
-        if name:
-            response = jsonify(name)
-            response.headers.add("Access-Control-Allow-Origin", "*")
-            return response
-    except:
-        pass
-    return "Bad request", "400"
+# @app.route("/api/name", methods = ["POST"])
+# def get_symbol_name():
+#     try:
+#         name = code_symbol.get_symbol_name(request)
+#         if name:
+#             response = jsonify(name)
+#             response.headers.add("Access-Control-Allow-Origin", "*")
+#             return response
+#     except:
+#         pass
+#     return "Bad request", "400"
 
-@app.route("/api/search", methods = ["POST"])
-def search():
-    try:
-        search_result = None
-        if request.data:
-            model = json.loads(request.data).get("model", "colBERT")
-            if model == "colBERT":
-                search_result = code_search_col_bert.search_for_text(request)
-            else:
-                search_result = code_search_code_bert.search_for_text(request)
-        if search_result:
-            response = jsonify(search_result)
-            response.headers.add("Access-Control-Allow-Origin", "*")
-            return response
-    except Exception as e:
-        print(str(e))
-        traceback.print_exc()
-        pass
-    return "Bad request", "400"
+# @app.route("/api/search", methods = ["POST"])
+# def search():
+#     try:
+#         search_result = None
+#         if request.data:
+#             model = json.loads(request.data).get("model", "colBERT")
+#             if model == "colBERT":
+#                 search_result = code_search_col_bert.search_for_text(request)
+#             else:
+#                 search_result = code_search_code_bert.search_for_text(request)
+#         if search_result:
+#             response = jsonify(search_result)
+#             response.headers.add("Access-Control-Allow-Origin", "*")
+#             return response
+#     except Exception as e:
+#         print(str(e))
+#         traceback.print_exc()
+#         pass
+#     return "Bad request", "400"
 
-@app.route("/api/search_index", methods = ["POST"])
-def search_index():
-    try:
-        if code_search_col_bert.indexing(request) != None:
-            return "Success", "200"
-    except Exception as e:
-        print(str(e))
-        traceback.print_exc()
-        pass
-    return "Bad request", "400"
+# @app.route("/api/search_index", methods = ["POST"])
+# def search_index():
+#     try:
+#         if code_search_col_bert.indexing(request) != None:
+#             return "Success", "200"
+#     except Exception as e:
+#         print(str(e))
+#         traceback.print_exc()
+#         pass
+#     return "Bad request", "400"
 
 
 if __name__ == "__main__":
@@ -98,7 +97,5 @@ if __name__ == "__main__":
     port = args.port
     debug = args.debug
     host = args.host
-
-    logging.debug = debug
 
     app.run(use_reloader=debug, port=port, debug=debug, host=host)
