ba5344a18c5bdbbbc347974df08ca5fff848281f
Laurin Stricker <laurin.stricker@gmail.com>
2022-05-06 16:20:54 +0200

    Changes for ironspeed.

--- controllers/code_search_codebert_controller.py
+++ controllers/code_search_codebert_controller.py
@@ -34,9 +34,9 @@ class CodeSearchCodeBertController:
         # code2_vec = model(tokenizer(code_2,return_tensors='pt').to(device).input_ids)[1]
         # code_3="hello world"
         # code3_vec = model(tokenizer(code_3,return_tensors='pt').to(device).input_ids)[1]
-        code_vecs=torch.cat(code1_vec,0) #,code2_vec,code3_vec
+        code_vecs=torch.cat((code1_vec),0) #,code2_vec,code3_vec
         codes = [code_1] #,code_2,code_3
-        scores=torch.einsum("ab,cb->ac",query_vec,code_vecs)
+        scores=torch.einsum("ab,cb->ac",query_vec,code1_vec)
         scores=torch.softmax(scores,-1)
         print("Query:",query)
         for i in range(3):
