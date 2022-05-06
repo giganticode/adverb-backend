8dc2d16a324b32b1d25acf1ac05d21a15508f4db
Laurin Stricker <laurin.stricker@gmail.com>
2022-05-06 16:17:02 +0200

    Changes for ironspeed.

--- controllers/code_search_codebert_controller.py
+++ controllers/code_search_codebert_controller.py
@@ -13,8 +13,12 @@ class CodeSearchCodeBertController:
         if not request.data:
             return None
 
-        data1 = json.loads(request.data)
-        content1 = data1.get("content", "")
+        data = json.loads(request.data)
+        content = data.get("content", "")
+        search_text = data.get("search", "")
+        batch_size = data.get("batch_size", 8)
+        if not content or not search_text:
+            return None
 
         device = "cuda:0" if torch.cuda.is_available() else "cpu"
         tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
@@ -22,9 +26,9 @@ class CodeSearchCodeBertController:
         model.to(device)
         # model = RobertaModel.from_pretrained("python_model")
 
-        query = "set a variable as hello world"
+        query = search_text
         query_vec = model(tokenizer(query,return_tensors='pt').to(device).input_ids)[1]
-        code_1="print('hello world')"
+        code_1=content[:512]
         code1_vec = model(tokenizer(code_1,return_tensors='pt').to(device).input_ids)[1]
         code_2="s = 'hello world'"
         code2_vec = model(tokenizer(code_2,return_tensors='pt').to(device).input_ids)[1]
@@ -34,42 +38,6 @@ class CodeSearchCodeBertController:
         codes = [code_1,code_2,code_3]
         scores=torch.einsum("ab,cb->ac",query_vec,code_vecs)
         scores=torch.softmax(scores,-1)
-        print("Query:",query)
-        for i in range(3):
-            print("Code:",codes[i])
-            print("Score:",scores[0,i].item())
-            
-            
-            
-        query = "Download an image and save the content in output_dir"
-        query_vec = model(tokenizer(query,return_tensors='pt').to(device).input_ids)[1]
-        code_1="""
-        def f(image_url, output_dir):
-            import requests
-            r = requests.get(image_url)
-            with open(output_dir, 'wb') as f:
-                f.write(r.content)
-        """
-        code1_vec = model(tokenizer(code_1,return_tensors='pt').to(device).input_ids)[1]
-        code_2="""
-        def f(image, output_dir):
-            with open(output_dir, 'wb') as f:
-                f.write(image)
-        """
-        code2_vec = model(tokenizer(code_2,return_tensors='pt').to(device).input_ids)[1]
-        # code_3="""
-        # def f(image_url, output_dir):
-        #     import requests
-        #     r = requests.get(image_url)
-        #     return r.content
-        # """
-        code_3 = content1[:512]
-        code3_vec = model(tokenizer(code_3,return_tensors='pt').to(device).input_ids)[1]
-        code_vecs=torch.cat((code1_vec,code2_vec,code3_vec),0)
-        codes = [code_1,code_2,code_3]
-        scores=torch.einsum("ab,cb->ac",query_vec,code_vecs)
-        scores=torch.softmax(scores,-1)
-        print("")
         print("Query:",query)
         for i in range(3):
             print("Code:",codes[i])
